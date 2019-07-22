c76473a01f46cb568f54409e2603ee9603e7fe58
alex martinez <almmartinez08@gmail.com>
2019-07-21 23:59:19 -0400

    add vaspene commands

--- intro_script.py
+++ intro_script.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 
 class SentdeBot(sc2.BotAI):
@@ -9,18 +9,37 @@ class SentdeBot(sc2.BotAI):
     await self.distribute_workers()
     await self.build_workers()
     await self.build_pylons()
+    await self.build_assimilators()
+    await self.expand()
 
   async def build_workers(self):
     for nexus in self.units(NEXUS).ready.noqueue:
       if self.can_afford(PROBE):
         await self.do(nexus.train(PROBE))
 
-  async def self.build_pylons(self):
+  async def build_pylons(self):
     if self.supply_left < 5 and not self.already_pending(PYLON):
       nexuses = self.units(NEXUS).ready
       if nexuses.exists:
         if self.can_afford(PYLON):
           await self.build(PYLON, near=nexuses.first)
+
+    async def build_assimilators(self):
+        for nexus in self.unit(NEXUS).ready():
+            vaspenes = self.state.vespene_geyser().closer_than(15.0, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+
+                worker = self.select_build_worker(vaspene.position)
+                if not worker:
+                    break
+
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build_assimilators())
+    async def expand(self):
+        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+            await self.expand_now()
 
 run_game(maps.get("AbyssalReefLE"),[
   Bot(Race.Protoss, SentdeBot()),
